320a5ed6de2a726eb6661a4d0c406bc362d56cfa
Kennedy Emeruem <kennedyemeruem123@gmail.com>
2022-06-10 17:32:57 -0700

    created the sheet class

--- sign_in.py
+++ sign_in.py
@@ -1,15 +1,39 @@
-import pygsheets as gc
+import pygsheets as pg
 import pandas as pd
 
-c = gc.authorize(service_file='./keys.json')
 
-df = pd.DataFrame(columns=['Name', 'Email', 'Password'])
+class Sheet:
+    def __init__(self, sheet_name: str):
+        '''
+        Initialize the google sheet
+        params: sheet_name: str'''
+        self.sheet_name = sheet_name
+        self.c = pg.authorize(service_file='keys.json')
+        self.sh = self.c.open(self.sheet_name)
 
-sheet_name = 'test_sheet'
-sheet_title = 'Sheet1'
+    def create_sheet(self, sheet_title: str, rows: int, cols: int) -> None:
+        '''
+        Create a new sheet in the google sheet
+        params: sheet_title: str, rows: int, cols: int
+        '''
+        self.sh.add_worksheet(sheet_title, rows=rows, cols=cols)
 
-sh = c.open(sheet_name)
-wks = sh.worksheet_by_title(sheet_title)
+
+    def get_sheet(self, sheet_title: str) -> pd.DataFrame:
+        '''
+        Get the dataframe of a sheet
+        params: sheet_title: str
+        '''
+        return pd.DataFrame(self.sh.worksheet_by_title(sheet_title).get_all_records())
+
+    def get_worksheet_list(self) -> list:
+        '''
+        Get the list of worksheet names
+        '''
+        self.sh.worksheets()
+        lists = []
+        for sheet in self.sh.worksheets():
+            lists.append(sheet.title)
+        return lists
 
 
-wks.set_dataframe(df, (1, 1))
