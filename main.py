16465110c60f50f89198dcb0168c1dc5198048ce
Kennedy Emeruem <kennedyemeruem123@gmail.com>
2022-06-18 14:12:05 -0700

    testing the classes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from sign_in import *
+from sheet_class import *
 
 
 
 if __name__ == '__main__':
     sh = Sheet('test_sheet')
-    print(sh.get_worksheet_list())
+    
+    sh.edit_col('test_sheet_2' ,5,'e', ['Kennedy', 'boys', 'look at me', 100, 40, 'Man', 'woman', 50, '50 cent'])
+    print(sh.get_sheet('test_sheet_2'))
